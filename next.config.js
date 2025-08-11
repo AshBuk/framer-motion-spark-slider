52682ea202002e69d8e0bc47a1c88a67a09b5fd0
Asher Buk <2asherbuk@gmail.com>
2025-08-11 20:51:57 +0300

    chore(next): remove custom webpack and unused image patterns

--- next.config.js
+++ next.config.js
@@ -1,46 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-
   images: {
     formats: ['image/webp', 'image/avif'],
-    deviceSizes: [640, 768, 1024, 1280, 1536],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '9000',
-        pathname: '/**',
-      },
-    ],
   },
-
-  experimental: {},
-
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-
-    config.optimization.splitChunks = {
-      chunks: 'all',
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    };
-
-    return config;
-  },
-
-  env: {},
 };
 
 module.exports = nextConfig;
