7b42c7cda90c106c37ced75df8b436789a51b44e
Asher Buk <2asherbuk@gmail.com>
2025-08-14 23:00:38 +0300

    chore(repo): symlink package sources to app src and enable transpilePackages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  transpilePackages: ['@ashbuk/spark-slider'],
   images: {
     formats: ['image/webp', 'image/avif'],
   },
